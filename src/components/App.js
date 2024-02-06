3ad88dfdeb6fadcced763fab0a50eb2f07547dc7
Dipak1121 <dipaks9604@gmail.com>
2024-02-06 14:44:37 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import data from '../Data/data'
+// import data from '../Data/data'
 
 const App = () => {
   return (
     <div id="main">
         <ol>
-            {
+            {/* {
                 data.map((item, index)=>{
                     return(
                         <li key={`relativeListItem${index+1}`}>{item}</li>
                     )
                 })
-            }
+            } */}
+            <li key="relativeListItem1">Moter</li>
+            <li key="relativeListItem2">Father</li>
+            <li key="relativeListItem3">Brother</li>
+            <li key="relativeListItem4">Sister</li>
+            <li key="relativeListItem5">Cousin</li>
          </ol>
     </div>
   )
